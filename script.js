b9bbb37efcbf8d551b2c8e0378abd16a04e28463
Akshita Gupta <gungunakshita37gupta973@gmail.com>
2025-08-14 07:26:25 +0530

    changing to ladakh

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
-const menuCloseBtn = document.querySelector('#menu-close-btn');
-const menuOpenBtn = document.querySelector('#menu-open-btn');
+const menuOpenButton = document.querySelector('#menu-open-btn');
+const menuCloseButton = document.querySelector('#menu-close-btn');
 
-menuOpenBtn.addEventListener('click', ()=> {
-    document.body.classList.toggle('show-mobile-menu')
-});
-menuCloseBtn.addEventListener('click', ()=> {
-    menuOpenBtn.click();
+// open menu when open button is clicked
+menuOpenButton.addEventListener('click', () => {
+    document.body.classList.toggle('show-mobile-menu'); // toggle is used to add or remove the class, if class is present it will remove it, if not present it will add it
+})
+// close menu when close button is clicked
+menuCloseButton.addEventListener('click', () => {
+    menuOpenButton.click(); // simulating a click on the open button to close the menu
 })
